5c2a4e0c9c36b90efe7e311ff3f53c6817b75526
Kamron Swingle <kamswingle@gmail.com>
2025-09-22 12:34:34 -0700

    Added resolution support, fullscreen checkbox, graphics settings, volume settings (master volume mixer).

--- Assets/Scripts/Menu/SettingsConfig.cs
+++ Assets/Scripts/Menu/SettingsConfig.cs
@@ -1,16 +1,62 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsConfig : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public AudioMixer audioMixer;
+    
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    
+    Resolution[] resolutions;
+    
+    // NOTE RESOLUTION AND FULLSCREEN SETTINGS WILL NOT SHOW UNLESS GAME IS BUILT
+
+    private void Start()
     {
+        resolutions = Screen.resolutions;
         
+        resolutionDropdown.ClearOptions();
+        
+        List<string> options = new List<string>();
+        
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetQuality(int qualityIndex)
     {
-        
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
